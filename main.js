e015d33ecbdf9e496c8aca86e42a2b57de6c0766
Crystalenium <pheonix1057@gmail.com>
2024-02-24 13:00:25 -0500

    checking

---
+++ main.js
@@ -0,0 +1,11 @@
+var bird = document.getElementById("bird")
+var bird_emoji = "**🐦**"
+
+bird.onclick = function(){
+		console.log(bird_emoji)
+		if (bird.innerHTML.includes(bird_emoji)){
+				bird.innerHTML = “bird”
+		} else {
+				bird.innerHTML = bird.innerHTML +bird_emoji
+		}
+}
